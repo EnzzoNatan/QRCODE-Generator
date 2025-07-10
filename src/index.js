d2ff99f14a676c3428a4073b07a1677f9a1f7526
EnzzoNatan <enzzoferrazz1909@gmail.com>
2025-07-10 05:37:01 -0300

    adicionando a logica da criação de senha

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import prompt from 'prompt'
 
 import mainPrompt from './prompts/prompt-main.js'
 import createQRCode from './prompts/qr-code/create.js'
+import createPassword from './prompts/password/create.js'
 
 
 async function main(){
     prompt.get(mainPrompt, async(err, result)=>{
         if(result.select == 1 ) await createQRCode();
-        if(result.select == 2 ) console.log('Escolheu SENHA')
+        if(result.select == 2 ) await createPassword();
     })
     
 prompt.start()
---
+++ src/services/password/create.js
@@ -0,0 +1,9 @@
+import chalk from "chalk"
+
+async function criarSenha(){
+    console.log(chalk.green("Senha criada com sucesso!"))
+}
+
+
+
+export default criarSenha
